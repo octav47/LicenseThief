c2c95e33ad10a4cec92b8e16cb141d044315e53c
tribunskiy <forty-7@yandex.ru>
2016-07-08 13:32:13 +0300

    update

--- pg.js
+++ pg.js
@@ -14,41 +14,85 @@ for (var k = 0; k < 11; k++) {
 
     for (var i in json) {
         if (json.hasOwnProperty(i)) {
-            (function (o) {
-                var id;
-                client.query('SELECT nextval(\'license_ids\')', function (err, result) {
-                    id = result.rows[0].nextval;
-
-                    for (var j in o) {
-                        if (o.hasOwnProperty(j) && j !== 'id' && j !== 'geomObj' && o[j] !== '') {
-                            o[j] = o[j].replace(/('|")/gmi, '');
-                        }
+            (function (o, i, k) {
+                var sqlQuery = 'SELECT license_local_id FROM license_local WHERE license_id = ' + o.id;
+                client.query(sqlQuery, function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        console.log(sqlQuery);
                     }
 
-                    var sqlQuery = 'INSERT INTO licenses VALUES(' + id + ', ' +
-                        '\'' + o.startDate + '\', ' +
-                        '\'' + o.regNumberSerial + '\', ' +
-                        '\'' + o.regNumber + '\', ' +
-                        '\'' + o.regNumberType + '\', ' +
-                        '\'' + o.owner + '\', ' +
-                        '\'' + o.purpose + '\', ' +
-                        '\'' + o.stateLicense + '\', ' +
-                        '\'' + o.place + '\', ' +
-                        '\'' + o.status + '\', ' +
-                        '\'' + o.stateOffice + '\', ' +
-                        '\'' + o.stateGov + '\', ' +
-                        '\'' + o.endDate + '\', ' +
-                        o.id + ')';
+                    console.log(result.rows.length);
+                    if (result.rows.length === 0) {
+                        var id;
+                        client.query('SELECT nextval(\'license_ids\')', function (err, result) {
+                            id = result.rows[0].nextval;
 
-                    var query = client.query(sqlQuery, function (err, result) {
-                        if (err) {
-                            console.error(err);
-                            console.log(sqlQuery);
-                        }
-                        //client.end();
-                    });
+                            for (var j in o) {
+                                if (o.hasOwnProperty(j) && j !== 'id' && j !== 'geomObj' && o[j] !== '') {
+                                    o[j] = o[j].replace(/('|")/gmi, '');
+                                }
+                            }
+
+                            var sqlQuery = 'INSERT INTO license_local VALUES(' + o.id + ', ' + id + ')';
+
+                            client.query(sqlQuery, function (err, result) {
+                                if (err) {
+                                    throw err;
+                                }
+
+                                var sqlQuery = 'INSERT INTO licenses VALUES(' + id + ', ' +
+                                    '\'' + o.startDate + '\', ' +
+                                    '\'' + o.regNumberSerial + '\', ' +
+                                    '\'' + o.regNumber + '\', ' +
+                                    '\'' + o.regNumberType + '\', ' +
+                                    '\'' + o.owner + '\', ' +
+                                    '\'' + o.purpose + '\', ' +
+                                    '\'' + o.stateLicense + '\', ' +
+                                    '\'' + o.place + '\', ' +
+                                    '\'' + o.status + '\', ' +
+                                    '\'' + o.stateOffice + '\', ' +
+                                    '\'' + o.stateGov + '\', ' +
+                                    '\'' + o.endDate + '\', ' +
+                                    o.id + ')';
+
+                                client.query(sqlQuery, function (err, result) {
+                                    if (err) {
+                                        console.error(err);
+                                        console.log(sqlQuery);
+                                    }
+                                    //client.end();
+                                });
+                            });
+                        });
+                    } else if (result.rows.length === 1) {
+                        var sqlQuery = 'INSERT INTO licenses VALUES(' + id + ', ' +
+                            '\'' + o.startDate + '\', ' +
+                            '\'' + o.regNumberSerial + '\', ' +
+                            '\'' + o.regNumber + '\', ' +
+                            '\'' + o.regNumberType + '\', ' +
+                            '\'' + o.owner + '\', ' +
+                            '\'' + o.purpose + '\', ' +
+                            '\'' + o.stateLicense + '\', ' +
+                            '\'' + o.place + '\', ' +
+                            '\'' + o.status + '\', ' +
+                            '\'' + o.stateOffice + '\', ' +
+                            '\'' + o.stateGov + '\', ' +
+                            '\'' + o.endDate + '\', ' +
+                            o.id + ')';
+
+                        client.query(sqlQuery, function (err, result) {
+                            if (err) {
+                                console.error(err);
+                                console.log(sqlQuery);
+                            }
+                            //client.end();
+                        });
+                    } else {
+                        throw 'find more then 1 rows in license_local_id; expected 1';
+                    }
                 });
-            })(json[i]);
+            })(json[i], i, k);
         }
     }
 }
